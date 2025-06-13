328d51fde17adf3470efde521c540254bd726564
SpeedOfMagic <vasilev.daniil18@gmail.com>
2025-06-13 15:55:41 +0000

    fix

--- src/atgen/utils/installers.py
+++ src/atgen/utils/installers.py
@@ -1,10 +1,10 @@
 import subprocess
 
 
-def install_spacy(self):
+def install_spacy():
     subprocess.run("python -m spacy download en_core_web_sm", shell=True)
 
-def install_nltk(self):
+def install_nltk():
     import nltk
     nltk.download('punkt')
     nltk.download('punkt_tab')
